9d57e680c21b6d2e5930b4a59ad51409a9f6a981
robertojvi <robertojvi@gmail.com>
2023-10-19 20:37:52 -0500

    Actividad Clase Variables

--- variables.js
+++ variables.js
@@ -1,2 +1,34 @@
-//Comentario
+// variables 
+
+// tipos de datos
+
+// STRINGS --> cadenas de texto
+
+let frase = "hola como estas?" // se puede utilizar comillas simples o dobles
+//para incluir comillas dentro del texto, debo utilizar diferentes tipos de comillas
+let fraseComilla = 'mi mama me dijo "como" vas'
+
+//también puedo usar números como string siempre que se use entre comillas
+
+// NUMBERS
+let altura = 30
+// el valor no va con comillas, se usa el . punto para decimales
+// como buena práctica, se definen como number los datos que necesito calcular matemáticamente, en el caso de un número telefónico o cédula, se usa string
+
+
+// BOOLEANS
+
+let estaLogueado = true
+
+//OPERADORES
+
+// TEMPLATE LITERALS
+    //para concatenar texto sin agregar espacios
+
+    console.log(`Hola ${altura} ${frase} como vas`)
+// matematicos  
+
+// comparacion
+
+// logicos
 
